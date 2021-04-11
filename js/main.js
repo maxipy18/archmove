4ee1273ee66a965b48768931570f0980f5930090
Maxim <maxipy@ya.ru>
2021-04-11 15:42:50 +0400

    add feedback section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
     $('.slider-box').slick({ 
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',       
@@ -7,4 +9,13 @@ $(document).ready(function(){
         infinite: false,
         variableWidth: true,        
     });
+
+    $('.feed-slider-box').slick({
+        
+        infinite: false,        
+        initialSlide: 0,
+        prevArrow: '<button type="button" class="slick-prev feed-btn"></button>',
+        nextArrow: '<button type="button" class="slick-next feed-btn"></button>',
+    });
+
   });
